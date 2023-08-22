8c2b96b74d3c5e54cf4ea52b3b9986125e06fb44
Harshil Solanki <harshilsolanki.ace@gmail.com>
2023-08-22 14:15:36 +0530

    Added env file and moved configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,20 +3,20 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { InventoryModule } from './inventory/inventory.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Brand } from './inventory/brand/brand.entity';
-import { Model } from './inventory/model/model.entity';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     InventoryModule,
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'harshil',
-      password: 'H@R7S@qE23',
-      database: 'inventory',
-      entities: [Brand, Model],
+      port: Number(`${process.env.DATABASE_PORT}`),
+      host: `${process.env.DATABASE_HOST}`,
+      username: `${process.env.DATABASE_USERNAME}`,
+      password: `${process.env.DATABASE_PASSWORD}`,
+      database: `${process.env.DATABASE_NAME}`,
+      entities: ["dist/**/*.entity{.ts,.js}"],
       synchronize: true,
     })
   ],
